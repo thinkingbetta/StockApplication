02ab014d541896b845d58aba9e5add789a251663
Elisabetta Spagnuolo <elisabetta.spagnuolo94@gmail.com>
2022-04-11 15:37:03 +0200

    DatabaseConnector added

--- src/main/java/com/accenture/stocks/StockApp.java
+++ src/main/java/com/accenture/stocks/StockApp.java
@@ -1,16 +1,32 @@
 package com.accenture.stocks;
 
 
-import com.accenture.stocks.cli.ScannerFormatting;
+import com.accenture.stocks.cliscanner.ScannerFormatting;
 import com.accenture.stocks.commands.Command;
 import com.accenture.stocks.commands.ExitCommand;
+import com.accenture.stocks.persistence.DatabaseConnector;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StockApp {
 
     private void starter() {
+
+        System.out.println("Welcome to StockApp!");
+
+        DatabaseConnector databaseConnector = new DatabaseConnector();
+        Connection connection = null;
+        try {
+            connection = databaseConnector.getConnection();
+        } catch (SQLException sqlException) {
+            System.out.println("Error in establishing a database connection.");
+            return;
+        }
+
+
         Scanner scanner = new Scanner(System.in);
         ScannerFormatting scannerFormatting = new ScannerFormatting(scanner);
 
@@ -19,7 +35,6 @@ public class StockApp {
         ArrayList<Command> commands = new ArrayList<>();
         commands.add(exit);
 
-        System.out.println("Welcome to StockApp!");
 
         boolean suspend = false;
         while (!suspend) {
---
+++ src/main/java/com/accenture/stocks/persistence/DatabaseConnector.java
@@ -0,0 +1,20 @@
+package com.accenture.stocks.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnector {
+    public Connection getConnection() throws SQLException {
+        Connection conn;
+        Properties properties = new Properties();
+        properties.put("user", "root");
+        properties.put("password", "secret");
+
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/salsasyntax", properties);
+
+        System.out.println("You are successfully connected to the database.");
+        return conn;
+    }
+}
