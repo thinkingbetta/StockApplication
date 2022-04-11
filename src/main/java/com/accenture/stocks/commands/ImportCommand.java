f8ae606c14474cb4ff977c55a058afe7f3e00b76
Elisabetta Spagnuolo <elisabetta.spagnuolo94@gmail.com>
2022-04-12 00:05:52 +0200

    Import class formatted

--- src/main/java/com/accenture/stocks/commands/ImportCommand.java
+++ src/main/java/com/accenture/stocks/commands/ImportCommand.java
@@ -7,8 +7,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -32,56 +30,35 @@ public class ImportCommand extends Command {
         System.out.println("Which file do you want to import? [d for default or type filename.csv]");
         String input = scanner.nextLine();
         if (input.equals("d")) {
-
-
             this.stocks = readStocksFromCsv(relativePath + defaultFile);
         } else {
-
-
             this.stocks = readStocksFromCsv(relativePath + input);
-
         }
-
         for (Stock stock : this.stocks) {
             System.out.println(stock.toString());
         }
-
         return value;
     }
 
-
     public ArrayList<Stock> readStocksFromCsv(String fileName) {
         ArrayList<Stock> stocks = new ArrayList<>();
-        Path pathToFile = Paths.get(fileName);
-
-        try (BufferedReader br = new BufferedReader(new FileReader(String.valueOf(pathToFile)))) {
+        try (FileReader fileReader = new FileReader(fileName)) {
+            BufferedReader br = new BufferedReader(fileReader);
             br.readLine(); //TODO controlla che la stringa iniziale sia corretta o if o eccezione personalizzata
             String line = br.readLine();
-
-            while (line  != null) {
+            while (line != null) {
                 String[] attributes = line.split(";");
-
-
                 Stock stock = this.stockService.createStock(attributes);
-
                 stocks.add(stock);
-
                 line = br.readLine();
             }
         } catch (FileNotFoundException fileNotFoundException) {
             System.out.println("Typo in the file name or check if file exists in data/ folder");
-
         } catch (IOException e) {
             e.printStackTrace();
         }
         return stocks;
     }
-
-
-
-
-
-
 
     @Override
     public String getName() {
